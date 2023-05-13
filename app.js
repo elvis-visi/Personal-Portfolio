4b85189524c22458d9a9aed78ed96e35b5539357
elvis-visi <sivisevi1@gmail.com>
2023-05-13 17:51:04 +0100

    26 Added dark theme.

--- app.js
+++ app.js
@@ -14,3 +14,29 @@ window.addEventListener('DOMContentLoaded', (event) => {
         });
     });
 });
+
+const toggleButton = document.getElementById('theme-toggle');
+
+const currentTheme = localStorage.getItem('theme'); //initially null
+console.log(currentTheme);  //  current theme stored in local storage
+
+if (currentTheme === 'dark') {
+  document.body.classList.add('dark');
+}
+
+toggleButton.addEventListener('click', function () {
+  //If the <body> currently has the dark class, it's removed; if it doesn't
+  // have the class, it's added.
+    document.body.classList.toggle('dark');
+
+  let theme = 'light';
+  if (document.body.classList.contains('dark')) {
+    theme = 'dark';
+  }
+  localStorage.setItem('theme', theme);  //store current theme in localStorage
+
+  console.log(document.body.classList);  //  log the classes on the body, including 'dark' if it's been added
+});
+
+
+
